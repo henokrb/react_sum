bcb71c6b4bac1ed767b6a3c3b708e7f61ed0c927
Heny <infowebet@gmail.com>
2025-02-12 14:40:04 +0300

    Solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,11 +1,5 @@
-// export const Sum = () => ();
-
-export const Sum = ({ a = 0, b = 0 }) => {
-  return (
-    <div>
-      <p>
-        Sum of {a} and {b} is {a + b}
-      </p>
-    </div>
-  );
-};
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>
+    Sum of {a} and {b} is {a + b}
+  </p>
+);
